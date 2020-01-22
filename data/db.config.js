62365debf166da2ef06d390e53f040e18b1d0f41
spettigrew <sara.pettigrew5@gmail.com>
2020-01-22 16:30:23 -0700

    DB config file setup

---
+++ data/db.config.js
@@ -0,0 +1,14 @@
+
+// ./data/dbConfig.js
+const knex = require('knex');
+
+const config = require('../knexfile.js');
+
+// if the environment variable is not set, default to 'development'
+// this variable is only set when running the "test" npm script using npm run test
+const dbEnv = process.env.DB_ENV || 'development';
+
+// the value of dbEnv will be either 'development' or 'testing'
+// we pass it within brackets to select the corresponding configuration
+// from knexfile.js
+module.exports = knex(config[dbEnv]);
