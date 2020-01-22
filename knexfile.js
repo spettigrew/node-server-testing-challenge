0bf4e21fea40295426cb92c0b0862da70d47b1d0
spettigrew <sara.pettigrew5@gmail.com>
2020-01-22 16:18:50 -0700

    Knexfile setup

---
+++ knexfile.js
@@ -0,0 +1,40 @@
+// Update with your config settings.
+
+
+// ./knexfile.js
+
+module.exports = {
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './data/user.db3',
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: './data/migrations',
+    },
+    seeds: {
+      directory: './data/seeds',
+    },
+    // needed when using foreign keys
+    pool: {
+      afterCreate: (conn, done) => {
+        // runs after a connection is made to the sqlite engine
+        conn.run("PRAGMA foreign_keys = ON", done); // turn on FK enforcement
+      },
+    },
+  },
+  testing: {
+    client: 'sqlite3',
+    connection: {
+      filename: './data/test.db3',
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: './data/migrations',
+    },
+    seeds: {
+      directory: './data/seeds',
+    },
+  },
+};
