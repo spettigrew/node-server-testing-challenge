49cb9a2b4f57686bb932346388e5f4640aff179d
spettigrew <sara.pettigrew5@gmail.com>
2020-01-22 21:22:53 -0700

    Return added

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const server = express();
 
 server.get('/', (req, res) => {
-    res.status(200).json({ api: 'running' });
+    return res.status(200).json({ api: 'running' });
 });
 
 module.exports = server;
