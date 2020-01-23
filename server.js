07bc5fdf0b99553173750d05cc5dd7f953bfd3ff
spettigrew <sara.pettigrew5@gmail.com>
2020-01-23 12:02:22 -0700

    Server file modified

--- server.js
+++ server.js
@@ -2,6 +2,14 @@ const express = require('express');
 
 const server = express();
 
+server.use(express.json())
+
+server.use((err, req, res, next) => {
+    console.log("Error:", err)
+    
+    return res.status(500).json({ message: "Something is wrong." ,})
+})
+
 server.get('/', (req, res) => {
     return res.status(200).json({ api: 'running' });
 });
