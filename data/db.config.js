f2b319325647e2d0c1c0081637e790d916289937
spettigrew <sara.pettigrew5@gmail.com>
2020-01-23 14:32:22 -0700

    Environment changed from dbEnv

--- data/db.config.js
+++ data/db.config.js
@@ -6,9 +6,11 @@ const config = require('../knexfile');
 
 // if the environment variable is not set, default to 'development'. dev = development
 // this variable is only set when running the "test" npm script using npm run test
-const dbEnv = process.env.NODE_ENV || 'dev';
+const environment = process.env.NODE_ENV || 'dev';
 
 // the value of dbEnv will be either 'development' or 'testing'
 // we pass it within brackets to select the corresponding configuration. Env = environment
 // from knexfile.js
-module.exports = knex(config[dbEnv]);
+module.exports = knex(config[environment]);
+
+// cross-env in package.json allows the environment to go across whatever platform your using. Mac, windows, linux, etc.
