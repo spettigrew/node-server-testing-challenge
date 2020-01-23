f879dcb11b35b6e392ae7951b4f94f126d954356
spettigrew <sara.pettigrew5@gmail.com>
2020-01-23 14:03:59 -0700

    Server file modified

--- server.js
+++ server.js
@@ -1,17 +1,23 @@
 const express = require('express');
 
+const critterRouter = require("./critters/critter-router")
+
 const server = express();
 
 server.use(express.json())
+
+require("dotenv").config()
+
+server.use("/critters", critterRouter)
+
+server.get('/', (req, res) => {
+    return res.status(200).json({ api: 'running' });
+});
 
 server.use((err, req, res, next) => {
     console.log("Error:", err)
     
     return res.status(500).json({ message: "Something is wrong." ,})
 })
-
-server.get('/', (req, res) => {
-    return res.status(200).json({ api: 'running' });
-});
 
 module.exports = server;
