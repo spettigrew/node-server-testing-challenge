5bb68951c0068f82b51f1b503402632172a1069d
spettigrew <sara.pettigrew5@gmail.com>
2020-01-22 21:24:51 -0700

    User model file initiated

---
+++ users/user-model.js
@@ -0,0 +1,34 @@
+
+const db = require("../data/db.config")
+
+async function insert(users) {
+    return null
+}
+
+async function update(id, changes) {
+    cont [id] = await db("user")
+    .insert(user, "id")
+    return db("users")
+    .where({ id })
+    .first()
+}
+
+function remove(id) {
+    return null
+}
+
+function getAll() {
+    return db("users")
+}
+
+function findById(id) {
+    return null
+}
+
+module.exports = {
+    insert,
+    update,
+    remove,
+    getAll,
+    findById,
+}
