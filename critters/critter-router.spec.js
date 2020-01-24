33f75d99b2437896bbf2ea7427a9584178c5debc
spettigrew <sara.pettigrew5@gmail.com>
2020-01-23 19:44:22 -0700

    Commented out console.log

--- critters/critter-router.spec.js
+++ critters/critter-router.spec.js
@@ -4,7 +4,7 @@ const server = require('../critters/critter-router')
 
 test("welcome route", async () => {
     const res = await supertest(server).get("/")
-    console.log(res)
+    // console.log(res)
 
     expect(res.status).toBe(200)
     expect(res.type).toBe("application/json")
