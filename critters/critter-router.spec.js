2d1f168997c694b669a00ff36a71fd6a41fbb527
spettigrew <sara.pettigrew5@gmail.com>
2020-01-23 22:04:44 -0700

    Added list and post to test file

--- critters/critter-router.spec.js
+++ critters/critter-router.spec.js
@@ -1,6 +1,10 @@
 const supertest = require("supertest")
 const server = require('../critters/critter-router')
+const db = require("../data/db.config")
 
+beforeEach(async () => {
+    await db.seed.run()
+})
 
 test("welcome route", async () => {
     const res = await supertest(server).get("/")
@@ -11,5 +15,28 @@ test("welcome route", async () => {
     expect(res.body.message).toBe("Welcome to Pine Needle Grove!")
 })
 
+test("get critter list", async () => {
+    const res = await supertest(server).get("/critters")
+    expect (res.status).toBe(200)
+    expect(res.type).toBe("application/json")
+    expect(res.body.length).toBeGreaterThan(0)
+    expect(res.body[0].id).toBe(1)
+    expect(res.body[0].name).toBe("George Mouse")
+})
 
-// Integration test
+test("create critter route", async () => {
+    const res = await supertest(server)
+    .post("/critters")
+    .send({ name: "Mrs. Wise" })
+
+    expect(res.status).toBe(201)
+    expect(res.type).toBBe("application/json")
+    expect(res.body).toEqual({ id: 7, name: "Mrs. Wise" })
+})
+
+
+
+
+// Integration test - supertest/endpoint test. Status code.
+
+// Notes for post endpoint integration test.
